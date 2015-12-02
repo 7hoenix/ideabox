ab9980011ac8ba542874edbe0ac56215242cecf1
philmphoenix <jpholzmann@gmail.com>
2015-12-01 18:16:32 -0700

    Adds create and destroy actions to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "site#index"
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      resources :ideas, only: [:index]
+      resources :ideas, only: [:index, :create, :destroy]
     end
   end
 end
