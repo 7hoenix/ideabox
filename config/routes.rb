860c3facbacd66b1ad8ebc21a39cfb6bf9d7b164
philmphoenix <jpholzmann@gmail.com>
2015-12-02 14:41:04 -0700

    Adds update action.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "site#index"
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      resources :ideas, only: [:index, :create, :destroy]
+      resources :ideas, only: [:index, :create, :destroy, :update]
     end
   end
 end
